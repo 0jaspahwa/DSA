309ff0651a4a7b4ee46132290b3343c662e081fd
0jaspahwa <ojaspahwa20@gmail.com>
2025-10-24 17:23:49 +0530

    Time: 80 ms (23.32%) | Memory: 135.8 MB (28.04%) - LeetSync

---
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        unordered_map<int, int> mp;
+        for(auto num : nums){
+            int r = ((num % value)+value)%value;
+            mp[r]++;
+        }
+        int MEX =0;
+        while(mp[MEX % value] > 0){
+            mp[(MEX % value)]--;
+            MEX++;
+        }
+        return MEX;
+    }
+};
