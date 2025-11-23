5e4296cd465cad25f230b9c48f567917d7c25b8d
0jaspahwa <ojaspahwa20@gmail.com>
2025-11-24 00:07:30 +0530

    Time: 43 ms (27.66%) | Memory: 42.5 MB (22.26%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int dp[40001][3];
-    int solve(vector<int>& nums, int i, int rem){
-        int n= nums.size();
-        if(i >= n){
-            if(rem == 0) return 0;
-            else{
-                return INT_MIN;
-            }
-        }
-        if(dp[i][rem] != -1){
-            return dp[i][rem];
-        }
-
-        int yes = nums[i] + solve(nums,i+1,(rem+nums[i]) % 3);
-        int no = solve(nums,i+1,rem);
-        return dp[i][rem] = max(yes,no);
-
-    }
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,0);
+        vector<vector<int>> t(n+1,vector<int>(3,INT_MIN));
+
+        t[n][0] = 0;
+        t[n][1] = INT_MIN; // index of out of bond
+        t[n][2] = INT_MIN;
+
+        for(int i=n-1; i>=0; i--){
+            for(int rem =0 ; rem <=2; rem++){
+                int newrem = (rem + nums[i]) % 3;
+                int take = t[i+1][newrem] == INT_MIN ? INT_MIN : (nums[i] + t[i+1][newrem]);
+                int skip = t[i+1][rem];
+
+                t[i][rem] = max(take,skip);
+            }
+        }
+        return t[0][0];
     }
 };
