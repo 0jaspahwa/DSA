e88498a81ffaed94dc246961e407120d44a1aaf1
0jaspahwa <ojaspahwa20@gmail.com>
2025-12-12 02:14:32 +0530

    Time: 94 ms (92.77%) | Memory: 92.8 MB (92.54%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int totalxor =0;
+        for(auto & num : nums){
+            totalxor ^= num;
+        }
+
+        int diff = (totalxor ^ k);
+
+        return __builtin_popcount(diff);
+    }
+};
