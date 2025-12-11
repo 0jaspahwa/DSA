57bef0b0cdfea1f75d7959e7dc2e321994c7a0cf
0jaspahwa <ojaspahwa20@gmail.com>
2025-12-11 20:40:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (98.51%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n =nums.size();
-        unordered_map<int,int> mp;
+        int result = 0;
 
-        for(auto &num : nums){
-            mp[num]++;
-        }
-
-        for(auto &i : mp){
-            if(i.second == 1){
-                return i.first;
+        for(int k=0; k<=31; k++){ // checking all 32 bits
+            int temp = 1 << k; //check whether the bit is 1 or not
+            int counto =0;
+            int countz =0;
+            for(auto &num : nums){
+                if( (num & temp) == 0){ 
+                    countz++;
+                }
+                else{
+                    counto++;
+                }
+            }
+            if(counto % 3 == 1){
+                result = (result | temp); // setting the bit 1 to get the number
             }
         }
-        return -1;
+        return result;
+        
     }
 };
