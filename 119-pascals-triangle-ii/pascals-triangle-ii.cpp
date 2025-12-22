3ebccdd9bc54789967dc4347730334a5b3dd047b
0jaspahwa <ojaspahwa20@gmail.com>
2025-12-23 02:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (50.38%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
+    typedef long long ll;
     vector<int> getRow(int rowIndex) {
-        //optimized we are using just one before elements to count the result
         int n = rowIndex;
-        vector<int> prev;
-            
-            for(int i=0; i<n+1; i++){
-                vector<int> curr(i+1,1);
-                for(int j=1; j<i; j++){
-                    curr[j] = prev[j] + prev[j-1];
-                }
-                prev = curr;
-            }
-        return prev;    
+        vector<int> result;
+        result.push_back(1);
+        for(int i=1; i<=n; i++){
+            ll element = (ll)(result[i-1]) * (n-i+1) / i;
+            result.push_back(element);
+        }
+        return result;
     }
 };
