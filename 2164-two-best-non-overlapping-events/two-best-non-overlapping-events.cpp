f8803d13c2bbac00bfaf16ae4563fdf65e0959f7
0jaspahwa <ojaspahwa20@gmail.com>
2025-12-24 04:00:20 +0530

    Time: 116 ms (47.13%) | Memory: 171.3 MB (50.82%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     vector<int> starts;
     int t[100001][3]; // [n][count]
+
+    int binarySearch(vector<vector<int>>& events, int endtime){
+        int n = events.size();
+        int l=0;
+        int r= n-1;
+        int result =n;
+
+        while(l <= r){
+            int mid = l +(r-l)/2;
+            if(events[mid][0] > endtime){
+                result = mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return result;
+    }
     int solve(vector<vector<int>>& events,int i, int count){
         int n = events.size();
         if(count == 2 || i>=n){
@@ -10,7 +29,7 @@ public:
         if(t[i][count] != -1){
             return t[i][count];
         }
-        int validIdx = upper_bound(starts.begin(),starts.end(),events[i][1]) - starts.begin(); //upper bound iterator deta hai isiliye -starts.begin kiya ki idx ki value mile
+        int validIdx = binarySearch(events, events[i][1]); //upper bound iterator deta hai isiliye -starts.begin kiya ki idx ki value mile
         int take = events[i][2] + solve(events,validIdx,count+1);
         int skip = solve(events,i+1,count);
         return t[i][count] = max(take,skip);
